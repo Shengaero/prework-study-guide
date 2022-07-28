c61c3d26b844fd8af3f9a99c9e39e4cdfe5d435e
Kaidan <kaidangustave@yahoo.com>
2022-07-28 13:48:29 -0500

    modified script for 3.3.4 and 3.3.5

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,26 @@
-var topics = "HTML, CSS, Git, JavaScript";
-console.log(topics);
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for(var i = 0; i < topics.length; i++) {
+    console.log(topics[i]);
+}
+
+/*
+switch(topic)
+{
+    case 'HTML':
+        console.log("Let's study HTML!");
+        break;
+    case 'CSS':
+        console.log("Let's study CSS!");
+        break;
+    case 'Git':
+        console.log("Let's study Git!");
+        break;
+    case 'JavaScript':
+        console.log("Let's study JavaScript!");
+        break;
+    default:
+        console.log('Please try again!');
+        break
+}
+*/
