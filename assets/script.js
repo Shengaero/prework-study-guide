5fdd00c080ac2ff3f9f293e29696789e2e5d0ddc
Kaidan <kaidangustave@yahoo.com>
2022-07-28 13:56:57 -0500

    added functions and notes on JS

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,33 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for(var i = 0; i < topics.length; i++) {
-    console.log(topics[i]);
+function listTopics() {
+    for(var i = 0; i < topics.length; i++) {
+        console.log(topics[i]);
+    }
 }
 
-/*
-switch(topic)
-{
-    case 'HTML':
-        console.log("Let's study HTML!");
-        break;
-    case 'CSS':
-        console.log("Let's study CSS!");
-        break;
-    case 'Git':
-        console.log("Let's study Git!");
-        break;
-    case 'JavaScript':
-        console.log("Let's study JavaScript!");
-        break;
-    default:
-        console.log('Please try again!');
-        break
+function selectTopic() {
+    switch(randomTopic)
+    {
+        case 'HTML':
+            console.log("Let's study HTML!");
+            break;
+        case 'CSS':
+            console.log("Let's study CSS!");
+            break;
+        case 'Git':
+            console.log("Let's study Git!");
+            break;
+        case 'JavaScript':
+            console.log("Let's study JavaScript!");
+            break;
+        default:
+            console.log('Please try again!');
+            break;
+    }
 }
-*/
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
